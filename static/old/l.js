24d312d027f21d97ed098708865e9b1bbe9c76c8
cinzy <33305897+cinzy@users.noreply.github.com>
2024-07-24 13:32:37 +0800

    Create l.js

---
+++ static/old/l.js
@@ -0,0 +1,238 @@
+function news()
+{
+//document.write("最近更新");
+n("2010-02-01","鼠标键盘动作脚本执行器","BETA 0.361","gems.htm#autoall","#ff4400");
+n("2009-12-13","鼠标键盘动作脚本执行器","BETA 0.36","gems.htm#autoall","#aaaaaa");
+n("2009-12-03","拼音打字修改辅助工具","BETA 0.5","gems.htm#InputAX","#00ff00");
+n("2009-07-30","鼠标键盘动作脚本执行器","BETA 0.353","gems.htm#autoall","#ff0000");
+n("2009-07-27","鼠标键盘动作脚本执行器","BETA 0.352","gems.htm#autoall","#AAAAAA");
+n("2009-07-25","鼠标键盘动作脚本执行器","BETA 0.351","gems.htm#autoall","#AAAAAA");
+n("2009-07-22","鼠标键盘动作脚本执行器","BETA 0.350","gems.htm#autoall","#AAAAAA");
+n("2009-07-02","鼠标键盘动作脚本执行器","BETA 0.340","gems.htm#autoall","#AAAAAA");
+n("2009-07-02","ESET NOD ID 获取器","BETA 0.50","gems.htm#GetNodId","#FF0000");
+n("2009-06-04","鼠标键盘动作脚本执行器","BETA 0.303","gems.htm#autoall","#AAAAAA");
+n("2009-06-02","鼠标键盘动作脚本执行器","BETA 0.302","gems.htm#autoall","#AAAAAA");
+n("2009-05-31","鼠标键盘动作脚本执行器","BETA 0.281","gems.htm#autoall","#ff0000");
+n("2009-05-26","ESET NOD ID 获取器","BETA 0.421","gems.htm#GetNodId","#FFFF00");
+n("2009-05-24","鼠标键盘动作脚本执行器","BETA 0.280","gems.htm#autoall","#AAAAAA");
+n("2009-05-20","ESET NOD ID 获取器","BETA 0.42","gems.htm#GetNodId","#AAAAAA");
+n("2009-05-15","ESET NOD ID 获取器","BETA 0.41","gems.htm#GetNodId","#AAAAAA");
+n("2009-05-14","键盘鼠标锁","演示版","demo/lock(input%20cinzy%20for%20unlock).rar");
+n("2009-05-13","ESET NOD ID 获取器","BETA 0.4 ","gems.htm#GetNodId","#AAAAAA");
+n("2009-05-05","CYMD5 计算工具","V0.2","soft.htm#CYMD5","#FF0000");
+n("2009-04-14","ESET NOD ID 获取器","BETA 0.3 ","gems.htm#GetNodId","#FF0000");
+n("2009-04-01","CyBoost (系统加速工具)","0.3","gems.htm#CyBoost","#FFff00");
+n("2009-03-30","鼠标键盘动作脚本执行器","BETA 0.260","gems.htm#autoall","#aaaaaa");
+n("2009-02-10","鼠标键盘动作脚本执行器","BETA 0.259","gems.htm#autoall","#aaaaaa");
+n("2009-01-13","鼠标键盘动作脚本执行器","BETA 0.258","gems.htm#autoall","#aaaaaa");
+n("2008-12-26","鼠标键盘动作脚本执行器","BETA 0.252","gems.htm#autoall","#aaaaaa");
+n("2008-12-16","鼠标键盘动作脚本执行器","BETA 0.243","gems.htm#autoall","#446644");
+n("2008-12-16","PDF password remover","V2.5 汉化完美版","rips.htm#PDFpsdrm");
+n("2008-12-15","更换博客主题","","/blog/","#0000ff");
+n("2008-12-12","博客建成","","/blog/","#0000ff");
+n("2008-12-09","鼠标键盘动作脚本执行器","BETA 0.242","gems.htm#autoall","#ff0000");
+n("2008-12-05","CyBoost (系统加速工具)","BETA 0.1","gems.htm#CyBoost","#00ff00");
+n("2008-12-03","鼠标键盘动作脚本执行器","BETA 0.241","gems.htm#autoall","#aaaaaa");
+n("2008-12-01","拼音打字修改辅助工具","BETA 0.3","gems.htm#InputAX","#00ff00");
+n("2008-11-28","拼音打字修改辅助工具","BETA 0.2","gems.htm#InputAX","#aaaaaa");
+n("2008-11-26","打字辅助工具","BETA 0.1","gems.htm#InputAX","#aaaaaa");
+n("2008-11-25","在线流行网络电视","(不卡的整理)","tv.asp","#0066ff");
+n("2008-11-21","打字辅助工具","(DEMO)","gems.htm#InputAX","#aaaaaa");
+n("2008-11-18","鼠标键盘动作脚本执行器","BETA 0.235","gems.htm#autoall","#ff0000");
+n("2008-11-17","鼠标键盘动作脚本执行器","BETA 0.233","gems.htm#autoall","#ff6600");
+n("2008-11-11","鼠标键盘动作脚本执行器","BETA 0.232","gems.htm#autoall","#aaaaaa");
+n("2008-11-07","鼠标键盘动作脚本执行器","BETA 0.231","gems.htm#autoall","#aaaaaa");
+n("2008-11-06","鼠标键盘动作脚本执行器","BETA 0.230","gems.htm#autoall","#ff0000");
+n("2008-10-21","鼠标键盘动作脚本执行器","BETA 0.229","gems.htm#autoall","#ff0000");
+n("2008-10-21","鼠标键盘动作脚本执行器","BETA 0.228","gems.htm#autoall","#aaaaaa");
+n("2008-10-20","鼠标键盘动作脚本执行器","BETA 0.225","gems.htm#autoall","#ff6600");
+n("2008-10-19","鼠标键盘动作脚本执行器","BETA 0.223","gems.htm#autoall","#ff6600");
+n("2008-10-17","鼠标键盘动作脚本执行器","BETA 0.221","gems.htm#autoall","#ff6600");
+n("2008-10-17","QQ自动登陆快捷方式创建器(QQ Auto Login)","V0.2","gems.htm#QAutoLog","#00ff00");
+n("2008-10-16","鼠标键盘动作脚本执行器","BETA 0.218","gems.htm#autoall");
+n("2008-10-16","鼠标键盘动作脚本执行器","BETA 0.215","gems.htm#autoall");
+n("2008-10-10","鼠标键盘动作脚本执行器","BETA 0.208","gems.htm#autoall");
+n("2008-09-28","鼠标键盘动作脚本执行器","BETA 0.206","gems.htm#autoall");
+n("2008-09-26","鼠标键盘动作脚本执行器","BETA 0.205","gems.htm#autoall");
+n("2008-09-24","ESET NOD ID 获取器","BETA 0.1 ","gems.htm#GetNodId");
+n("2008-09-19","鼠标键盘动作脚本执行器","BETA 0.202","gems.htm#autoall");
+n("2008-09-18","鼠标键盘动作脚本执行器","BETA 0.2","gems.htm#autoall");
+n("2008-09-12","鼠标键盘动作脚本执行器","BETA 0.1","gems.htm#autoall");
+n("2008-09-03","鼠标键盘动作脚本执行器","Alpha","gems.htm#autoall");
+n("2008-07-30","Proxy++(自动设置IE代理)","Alpha","gems.htm#Proxypp");
+n("2008-07-20","修正网站在firefox下显示错误");
+n("2008-07-18","网站全新改版");
+n("2008-06-16","QQ自动登陆快捷方式创建器(QQ Auto Login)","V0.1","gems.htm#QAutoLog");
+n("2008-06-16","Recuva完全汉化版","","gems.htm#comdbg");
+n("2008-06-11","便携串口调试助手","V0.3 RC","electorns.htm#comdbg");
+n("2008-06-06","KEIL UV3 绿色精简版","RC2","rips.htm#keiluv3");
+n("2008-06-03","HEX2ASMI 51反汇编工具智能版","V0.1","electrons.htm#hex2asmI");
+n("2008-06-02","KEIL UV3 绿色精简版","RC1","rips.htm#keiluv3");
+n("2008-06-01","KEIL UV3 绿色精简版","Alpha","rips.htm#keiluv3");
+n("2008-05-04","PDF password remover","V1.5.327 汉化特别版","rips.htm#PDFpsdrm");
+n("2008-03-09","MD5 计算工具","V0.1","gems.htm#CyMd5");
+n("2008-03-09","便携串口调试助手","V0.2 RC","electorns.htm#comdbg");
+n("2008-03-07","正弦波表生成器","V1.3","electorns.htm#VsinTab");
+n("2008-02-18","便携串口调试助手","VBETA0.1","electorns.htm#comdbg");
+n("2008-01-30","便携串口调试助手","Alpha","electorns.htm#comdbg");
+n("2008-01-28","串并口监视软件 portmon","汉化版","gems.htm#portmon");
+n("2006-08-17","HEX2ASM 51反汇编工具","V06.8.17","electorns.htm#hex2asm");
+}
+function electorns()
+{
+//s(标签,连接,图片,名称,大小,日期,说明,MD5)
+s("comdbg","便携串口调试助手(COMDBG) <font color=#ff0000>精品</font>","0.3 RC","5.19K","2008/6/11","串口调试软件中的一员,不同的是此版本纯API编写,运行高效稳定,只有5K,体系非常小巧,功能强大,非常值得收藏<br>--------------------<br>更新记录<br>0.3RC<br>◎改变回车输入方式(Ctrl+Enter改成Enter)<br>0.2RC<br>◎修改几处内部逻辑使程序运行更快","AC96891910E99CD82DC89EA5198B887A","/soft/COMdbg.rar",1,"\
+ | <a href=http://www.skycn.com/soft/45813.html target=_blank>天空下载</a>\
+ | <a href=http://www.xdowns.com/soft/38/63/2008/Soft_43975.html target=_blank>绿盟下载</a>\
+");
+
+s("hex2asm","HEX2ASM 51反汇编工具","06.8.17","375K","2006/8/19",
+"简介:hex2asm是51单片机系列的反汇编工具,强大的数据与指令地址的分离,独特的人工建立数据表功能使得反汇编过程更灵活,准确,人性化界面更加易于操作<br>\
+--------------------<br>PS:早期作品,Delphi作品,比较臃肿<br>--------------------<br><input onclick=this.value=\"请在最上面小窗口输入:cinzy\" value=点击获取邀请码 type=button><br>--------------------<br>更新记录<br>2008/6/12日修正获取邀请码的连接(为更改原文件创建日期)",
+"AA6A7B269808EF2C12CB9FBBC85141E2","/soft/hex2asm.rar",5,
+" | <a href=http://down.cnzz.cn/info/43233.aspx target=_blank>站长下载</a>");
+
+s("hex2asmI","HEX2ASMI 51反汇编工具智能版","V0.1","8K","2008/6/3",
+"简介:用法同上软件,VC++重写代码,加入只能翻译功能,目前仅作测试<br>",
+"9D7DA7817E1B41DB9EB33A70FF57FABE","/soft/hex2asmI.rar",3,"");
+
+s("VsinTab","正弦波表生成器","V1.3","5.12K","2008/3/7","简介:主要用于MCU产生Vsin,查表产生正弦波<br>\
+1.PWM输出最好采用正半周期,PWM选两路交替查表,用桥臂电路进行功率放大<br>\
+2.DA输出可以使用产生全波形<br>\
+","7EAE7B67DD22C0B91408CDE5A2CCFEEE","/soft/VsinTab.rar",4,"");
+}
+
+function gems()
+{
+//document.write("<div style=\"width:550px;font:12px 宋体;\">本页为精品软件区</div>");
+//s(标签,连接,图片,名称,大小,日期,说明,MD5)
+s("autoall","鼠标键盘动作脚本执行器 AutoAll","0.361","25.4K","2010-02-01",
+"简介:<br>\
+创建脚本文件让电脑自动工作,解放双手<br>\
+--------------------<br>\
+V0.361  BETA 0201<br>\
+[*]修复热键执行脚本指令在保存exe后失效的bug<br>\
+V0.36  BETA 1213<br>\
+[*]优化脚本执行效率<br>\
+[+]加载脚本时对脚本语言判断<br>\
+V0.353 BETA 0730<br>\
+◎[修复]热键则执行其他脚本的选择脚本出现乱码的bug<br>\
+◎[修复]进程移至前台则继续在XP下无法获取进程名字的bug<br>\
+V0.352 BETA 0727<br>\
+◎[修复]鼠标移动到托盘图标消失的bug<br>\
+◎[修复]鼠标新版本图标<br>\
+◎[修复]定位窗口到左上角不能插入的bug<br>\
+◎[改进]优化内存占用(原来3M左右现在1M左右)<br>\
+V0.351 BETA 0725<br>\
+◎[修复]勾选直接运行与循环执行的bug<br>\
+◎[修复]利用剪贴板输出不能长时间运行的bug<br>\
+V0.350 BETA 0722<br>\
+◎[改进]输出的EXE文件改为无，壳支持修改图标和再次压缩<br>\
+◎[改进]点击测试不再提示保存脚本<br>\
+◎[增加]支持显示托盘图标<br>\
+◎[增加]支持exe类型文件拖拽到窗口<br>\
+BETA 0.340  0702<br>\
+◎[增加]命令列表下拉框增加提示<br>\
+◎[增加]增加脚本启动提示信息自定义功能<br>\
+◎[增加]WinIO支持（仅支持PS2键盘）<br>\
+◎[修复]修复长时间运行的不稳定性<br>\
+◎[修复]修复其他细微bug<br>\
+BETA 0.303  0604<br>\
+◎修复列表框双击删除条目跳转到第一行的bug<br>\
+<a href=http://blog.cinzy.com/AutoAll/ style=color:#ff0000>[更多]</a><br>\
+本软件杀软有误报,联系CINZY所要无壳版\
+<br>\
+<a href=\"/count/?n=13&url=/soft/autoallold.rar\">下载早期版本</a>\<br>\
+","C34456B4923CB0EC21B713127FD06767","/soft/autoall.rar",13,"\
+ | <a href=http://www.greendown.cn/soft/11688.html target=_blank>绿色下载站</a>\
+ | <a href=http://www.skycn.com/soft/48217.html target=_blank>天空载站</a>");
+ 
+s("inputax","拼音打字修改辅助工具","BETA 0.5","48.8K","2009/12/03",
+"简介:修改已上屏的文字<br><br>\
+BETA 0.5  2009/12/03<br>\
+◎加入对windows7的支持<br><br>\
+PS:微软拼音在word中的热键WIN+C可以修改文字读音相同但字不同的文字，这个软件实现的就是任何一个输入法任何程序中实现word中的WIN+C<br><br>\
+使用方法:打开软件选中要修改的部分按动热键win+z<br><br>\
+　　比如说“打字”一不小心上屏为“大字”，这时可以选中“大字”按动默认热键win+z即可修改“大字”为“打字”<br><br>\
+　　可以做纯绿色版，在桌面或开始菜单建立一个快捷方式,在快捷方式属性里的目标后面加个参数 -s 设置一个快捷键，不仅实现了自定义热键而且还不占系统内存,绿色运行<br><br>\
+下图为演示动画：<img src=images/InputAX演示.gif>",
+"B2232353C82E53F8DFDF0B0EFD2EF83A","/soft/InputAX.rar",15,"");
+
+s("cyboost","CyBoost (系统加速工具)","V0.3 无壳","3.5KB","2009/04/1",
+"简介:功能相当精简体积相当小的类似PC boost得软件 通过调度系统优先级加速程序运行速度<br>\
+V0.3 [0401] 修改为程序仅运行一次(愚人节不愚人)<br>\
+--------------------<br>\
+无界面,双击打开即可鼠标变为等待形状即可<br>\
+长按Ctrl + LWIN + Alt 一秒结束程序",
+"D3826275179CC72F3F5388E3986E619F","/soft/CyBoost.rar",17,"");
+
+s("qautolog","QQ自动登陆快捷方式创建器(QQ Auto Login)","0.2","6KB","2008/10/17",
+"简介:<br>可以用来创建QQ,TM,QQ游戏等自动登陆的快捷方式,免去输入密码提高安全性,附加强行聊天功能,适合一人有多QQ个用户使用<br>\
+--------------------<br>\
+PS:如果不相信软件安全性可以随意输入QQ号码,然后在生成的快捷方式属性里面替换成为您的QQ号码<br>\
+--------------------<br>\
+更新记录<br>\
+V0.2<br>\
+◎加入安全提示\
+◎更改测试QQ的密码<br>\
+◎浏览键增加功能<br>\
+V0.1<br>◎修正自定义桌面无法创建快捷方式的bug",
+"E272525B7AF1D8E8CAB878B5D47A1D1C","/soft/Qautolog.rar",11,"");
+
+s("getnodid","ESET NOD ID获取器(支持NOD32 4.0版本)","V0.50","5KB","2009-7-02",
+"简介:NOD的辅助工具,用于获取用户名和密码,可设置为自动执行<br>\
+<font color=ff0000>被360盯上了,大家低调使用吧,别去NOD、360等论坛发贴了</font><br>\
+--------------------<br>\
+更新记录<br>\
+【2009-7-02】V0.50<br>\
+  解决被查封<br>\
+【2009-5-26】V0.421<br>\
+  解决360检测为木马的问题<br>\
+【2009-5-20】V0.42 <br>\
+  1.逻辑处理优化<br>\
+【2009-5-15】V0.41 <br>\
+  1.增加更多类型ID(如TRIAL,ESS等)<br>\
+  2.增加自动更新<br>\
+  3.修复启动时偶尔无法更新ID的bug<br>\
+【2009-5-13】V0.4 <br>\
+  1.增加对4.0版本的支持<br>\
+【2009-4-14】V0.3 <br>\
+  ◎1.改变服务器为cinzy.com<br>\
+  ◎2.比V0.2启动速度提高50倍<br>\
+【2008-9-25】V0.2 <br>\
+  ◎1.修正个别时刻获取错误ID<br>\
+  ◎2.优化开机启动加入延时以等待连接网络<br>\
+【2008-9-20】V0.1 <br>\
+  ◎加入开机自动运行功能",
+"7980A3A83F2009CCF2F0BB96EAD117C1","/soft/GetNodId.rar",14,"");
+
+s("proxypp","Proxy++(自动设置IE代理)","alpha","5.36KB","2008/7/30","简介:获取代理列表,设置IE代理","604FCE71BC6F8F52C9F31A0A6293BFAE","/soft/Proxypp.rar",1,"");
+
+s("xxview","星号密码查看器","V0.1","20K","2008/7/25","简介:察看星号窗口密码","F1ACAA1B181EA166D2AD9A1B381C9E79","/soft/xxview.rar",16,"");
+
+s("cymd5","MD5 计算工具","V0.2","5.48K","2009/5/5","简介:要知道下载的文件是否安全,各大下载站都有MD5校验的一串字符,本程序就是计算MD5字符串以和网站发布的比较来判断文件是否被恶意篡改,支持托拽<br>V0.2更新[2009-5-5]<br>◎优化计算速度<br>◎增加时间显示<br>◎支持无穷大文件<br>V0.1最初版 时间2008/3/9 大小7.62K","4B9B7A4562ADDDF383216EB8EFB77F1C","/soft/CyMd5.rar",7,"");
+
+}
+function rips()
+{
+document.write("<div style=\"background-color:#ff0000;color:#ffffff;width:550px\">&nbsp;本页软件仅供学习研究使用,请下载后24小时内删除</div>");
+//s(标签,连接,图片,名称,大小,日期,说明,MD5)
+s("portmon","串并口监视软件 portmon (Chinesize)","Alpha","78.97K","2008/9/3","说明:监视串口收发数据,不占用串并口,非调试助手","46F768FA289990AEB165431ACF85BB24","/soft/portmon.rar",6);
+s("keiluv3","KEIL UV3 for 51 绿色精简版(green)","RC2","3.98M","2008-06-06",
+"说明:KEIL UV3 for 51是51单片机非常好的IDE,原版的有17M之多,精简后内核采用UV3 V3.31,支持全部51核心芯片,支持RTOS仿真,功能没有衰减<br>\
+--------------------<br>\
+精简内容:<br>\
+◎1.精简帮助文档<br>\
+◎2.修正乱码,难看的\"礦\"字不在出现在标题栏<br>\
+◎3.可扩展ARM编译器增加对ARM编程功能<br>\
+--------------------<br>\
+历史更新:<br>\
+RC2:完全自动安装点击目录下初始化.bat即可,自动对tools.ini进行配置,初学者更加实用\
+RC1:继续精简,从4.98MB精简到3.98MB<br>\
+Alpha:精简到4.98MB,去除帮助文档(如有需要可单独下载)<br>(声明:本程序仅共个人学习所用,请于24小时删除)",
+"35C6F6B2FBEE79D4AA1893810E1A002E","/soft/keil.rar",10);
+s("PDFpsdrm","PDF password remover 解决PDF文件不能复制问题 (rips)","V2.5","165KB","2008-12-16","特别版完全汉化，做论文的工程师必备 完美版","-","/soft/PDF password remover.rar",8,"<br><br><a href='count/?n=8&url=/soft/PDF password remover3.0.rar' target=_blank>【3.0版本下载】(2.5版本因为密码为空则提示无法解密可以使用这个版本，这个版本不是很完善退出后会出现内存泄漏，但不影响使用)</a>");
+s("Recuva","Recuva完全汉化版 (Chinesize)","V1.5.327","1.01MB","2008-06-16","说明:找回清空回收站里的数据,效果同类软件中最好,对保密数据进行擦除,美国国防部的擦除技术数据不会外泄,公司有保密数据时不能错过<br>\
+因官方简体中文版汉化不全，站长做此版本方便大家使用\
+","4D47BB072892420BE983D8084732CFC2","/soft/Recuva.rar",9);
+}
